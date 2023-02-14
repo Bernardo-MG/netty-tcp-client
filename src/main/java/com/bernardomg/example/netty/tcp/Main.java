2ba86d7d75c289913eaada5e3fa89b4e0008b77d
Bernardo-MG <programming@bernardo-mg.com>
2023-02-14 17:43:53 +0100

    Using Lombok logger

--- src/main/java/com/bernardomg/example/netty/tcp/Main.java
+++ src/main/java/com/bernardomg/example/netty/tcp/Main.java
@@ -1,26 +1,20 @@
 
 package com.bernardomg.example.netty.tcp;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.bernardomg.example.netty.tcp.cli.TcpClientMenu;
 
+import lombok.extern.slf4j.Slf4j;
 import picocli.CommandLine;
 
+@Slf4j
 public class Main {
-
-    /**
-     * Logger.
-     */
-    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
     public static void main(final String[] args) {
         final Integer exitCode;
 
         exitCode = new CommandLine(new TcpClientMenu()).execute(args);
 
-        LOGGER.debug("Exited with code {}", exitCode);
+        log.debug("Exited with code {}", exitCode);
 
         System.exit(exitCode);
     }
